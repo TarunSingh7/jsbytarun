bb16bdc0b0c395e1991b1f69c379dec36e3208f1
TarunSingh <tarunsingh945748@gmail.com>
2024-09-18 23:28:18 +0530

    Added chapter2.js file

---
+++ chapter2.js
@@ -0,0 +1,7 @@
+const id = 121;
+let name = "John";
+var accountnumber = "1392124";
+
+console.log(id);
+//we cant assgin the value again in the chapter2
+//we are not using the var beacuse it is confusing in the scope
