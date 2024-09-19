d0cdd579c7c4a76c9bd327042845c3c688a3db40
TarunSingh <tarunsingh945748@gmail.com>
2024-09-19 11:45:46 +0530

    this is third fiile

---
+++ chapter3.js
@@ -0,0 +1,14 @@
+//conversion in js
+let score = "3abc";
+
+console.log(typeof score);
+
+let value = Number(score); // number mein convert hogya
+console.log(typeof value);
+console.log(value); //Nan is the not a number
+
+//convert the number
+
+let x = 0;
+let y = Boolean(x);
+console.log(y);
